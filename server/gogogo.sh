28e300c3142e6aaf12bdcd92f655e1f83f9e844b
kakaruoter <ej1083233@swccd.edu>
2019-08-20 23:14:10 +0800

    v2ray

--- server/gogogo.sh
+++ server/gogogo.sh
@@ -2,7 +2,8 @@
 #v2ray反向代理脚本服务器端
 #v1.0 by kakaruoter 2019.8.20
 
-read -p "请输入服务器防火墙中已添加的端口号：" pt
+read -p "请输入服务器防火墙中已添加的一个端口号：" pt1
+read -p "请输入第二个已添加的端口号：" pt2
 
 wget https://install.direct/go.sh
 
@@ -12,19 +13,27 @@ chmod +x go.sh
 #此乃v2ray官方脚本
 
 uuid1=`/usr/bin/v2ray/v2ctl uuid`
+uuid2=`/usr/bin/v2ray/v2ctl uuid`
 
-sed -ri '/"port":/c\      "port":'$pt',' /root/sys_config.txt
+sed -ri '14s/.*/      "port":'$pt1',/' /root/sys_config.txt
 
-sed -ri '/"id":/c\            "id":'$uuid1',' /root/sys_config.txt
+sed -ri '19s/.*/          "id":'$uuid1',/' /root/sys_config.txt
+
+sed -ri '28s/.*/      "port":'$pt2',/' /root/sys_config.txt
+
+sed -ri '33s/.*/          "id":'$uuid2',/' /root/sys_config.txt
 
 cat sys_config.txt >/etc/v2ray/config.json
 
 clear
 
 echo ""
-echo "请确定端口已添加到防火墙：$pt"
+echo "请为内网服务器json文件配置此端口：$pt2"
 echo ""
-echo "您的uuid是：$uuid1"
+echo "请为内网服务器json文件配置此uuid：$uuid2"
 echo ""
-echo "服务端配置已完成，请完善内网服务器配置！"
+echo "外网终端请配置此端口：$pt1"
+echo ""
+echo "外网终端请配置此uuid：$uuid1" 
+echo "服务端配置已完成，请确保所有端口已添加到防火墙"
 
